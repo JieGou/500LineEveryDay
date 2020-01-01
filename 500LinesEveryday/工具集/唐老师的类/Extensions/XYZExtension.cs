48743bc7278f9b2f2931f00d06692860f0780e17
295665210 <295665210@qq.com>
2020-01-01 12:59:27 +0800

    2020年1月1日12:59:15 https://github.com/295665210/500LinesEveryday.git day1

---
+++ 500LinesEveryday/工具集/唐老师的类/Extensions/XYZExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace 唐老师的类
+{
+    public static class XYZExtension
+    {
+        public static XYZ xyComponent(this XYZ po)
+        {
+            return new XYZ(po.X, po.Y, 0);
+        }
+
+        public static XYZ getRandomNorm(this XYZ vec)
+        {
+            XYZ norm = new XYZ(-vec.Y + vec.Z, vec.X + vec.Z, -vec.Y - vec.X);
+            return norm.Normalize();
+        }
+    }
+}
---
+++ 500LinesEveryday/工具集/唐老师的类/Helpers/collectorHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace 唐老师的类
+{
+    public static class CollectorHelper
+    {
+        public static IList<T> TCollector<T>(this Document doc)
+        {
+            Type type = typeof(T);
+            return new FilteredElementCollector(doc).OfClass(type).Cast<T>().ToList();
+        }
+    }
+}
