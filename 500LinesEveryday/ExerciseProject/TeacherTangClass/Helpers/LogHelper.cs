7f678681cfd9dcc4982b68aa167bebe571c34a75
295665210 <295665210@qq.com>
2020-01-01 21:07:03 +0800

    修改为区分文件名大小写

---
+++ 500LinesEveryday/ExerciseProject/TeacherTangClass/Helpers/LogHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherTangClass
+{
+    public static class LogHelper
+    {
+        public static void LogException(Action action, string path)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                LogWrite(e.ToString(), path);
+            }
+        }
+
+        public static void LogWrite(string msg, string path)
+        {
+            StreamWriter sw = new StreamWriter(path);
+            sw.WriteLine(msg);
+            sw.Close();
+        }
+    }
+}
