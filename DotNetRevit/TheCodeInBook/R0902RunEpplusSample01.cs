4a4ed3adc7d759b6d3a5d378a29f8eeb70f603fb
295665210 <295665210@qq.com>
2020-01-10 11:40:41 +0800

    2020年1月10日11:40:35中午

---
+++ DotNetRevit/TheCodeInBook/R0902RunEpplusSample01.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Myclass;
+using TeacherTangClass;
+using View = Autodesk.Revit.DB.View;
+using MyClass;
+using Form = Autodesk.Revit.DB.Form;
+
+namespace ExerciseProject
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    [Journaling(JournalingMode.UsingCommandData)]
+    class R0902RunEpplusSample01 : IExternalCommand
+    {
+        /// <summary>
+        /// 代码片段4-4
+        /// 创建轴网
+        /// </summary>
+        /// <param name="commandData"></param>
+        /// <param name="message"></param>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Selection sel = uidoc.Selection;
+            View acView = uidoc.ActiveView;
+
+            RunEpplusSample.RunSample1();
+            TaskDialog.Show("Tips", "我太难了,使用Epplus生成了一个xls文档");
+
+
+            Transaction ts = new Transaction(doc, "******");
+
+            try
+            {
+                ts.Start();
+
+                ts.Commit();
+            }
+            catch (Exception)
+            {
+                if (ts.GetStatus() == TransactionStatus.Started)
+                {
+                    ts.RollBack();
+                }
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
