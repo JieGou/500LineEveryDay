76a2f057005bb0fc69c2fe556288e2e8606aa13b
295665210 <295665210@qq.com>
2020-01-11 20:40:39 +0800

    老师的框架,做成循环

--- DotNetFoundation/FConsoleFramWork/App.cs
+++ DotNetFoundation/FConsoleFramWork/App.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FConsoleMain
@@ -11,58 +13,57 @@ namespace FConsoleMain
     {
         static void Main(string[] args)
         {
-            int i = 200;
-            string s1 = "这是一个字符串"; //声明变量
-            //输出变量到控制台
-            Console.WriteLine("输入要执行的程序文件名:");
-            var inputstr = Console.ReadLine();
+            ConsoleKey key;
 
-            //var  parseResult= int.TryParse(inputstr, out int result);
-
-
-            if (true)
+            do
             {
+                Console.WriteLine("输入要执行的程序文件名:");
+                var inputstr = Console.ReadLine();
                 var asm = Assembly.GetExecutingAssembly();
                 var types = asm.GetTypes();
+                int num = 0;
 
                 foreach (var type in types)
                 {
                     Console.WriteLine(type.Name);
+                }
 
-                    if (type.Name.ToLower().Contains(inputstr.ToLower()))
+                foreach (var type in types)
+                {
+                    if (inputstr.ToLower() == type.Name.ToLower())
                     {
-                        var methods = type.GetMethods(BindingFlags.NonPublic|BindingFlags.Static);
-                          
-                        var targetMethod = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Static).Where(m => m.IsStatic && m.Name.ToLower().Contains("main")).FirstOrDefault();
+                        var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Static);
+
+                        var targetMethod = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Static)
+                            .Where(m => m.IsStatic && m.Name.ToLower().Contains("main")).FirstOrDefault();
                         Console.WriteLine(targetMethod.Name);
 
-                        var objarr = new object[1]  ;
+                        var objarr = new object[1];
                         var obj = asm.CreateInstance(type.FullName);
 
-                        Console.WriteLine("type.FullName:" +type.FullName+"\n程序结果如下:\n\n");
+                        Console.WriteLine("type.FullName:" + type.FullName + "\n程序结果如下:\n\n");
+
                         if (obj == null)
                         {
                             Console.WriteLine("对象未能创建，将退出程序");
                             Console.ReadKey();
                             return;
-
                         }
 
+                        num++;
                         targetMethod.Invoke(obj, objarr);
-
                     }
-
                 }
-            }
-            else
-            {
-                
-            }
 
-            Console.WriteLine(i);
-            Console.WriteLine(s1);
-            Console.ReadKey();
+                if (num == 0)
+                {
+                    Console.WriteLine("输入错误");
+                }
 
+                Console.WriteLine("请按任意键重新重新输入,或按esc退出");
+                key = Console.ReadKey().Key;
+
+            } while (key != ConsoleKey.Escape);
         }
     }
 }
--- DotNetFoundation/FConsoleFramWork/F1001Que15.cs
+++ DotNetFoundation/FConsoleFramWork/F1001Que15.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace F0901C04S06
+namespace FConsoleMain
 {
 class F1001
     {
