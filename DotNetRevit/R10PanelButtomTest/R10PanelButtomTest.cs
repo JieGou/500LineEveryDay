da4ff8dbb25bdce14f16069d5c3a207e7852e113
295665210 <295665210@qq.com>
2020-01-12 21:09:29 +0800

    2020年1月12日21:09:24 晚上

--- DotNetRevit/R10PanelButtomTest/R10PanelButtomTest.cs
+++ DotNetRevit/R10PanelButtomTest/R10PanelButtomTest.cs
@@ -10,6 +10,7 @@ using Autodesk.Revit.UI.Events;
 using Autodesk.Revit.UI.Selection;
 using System.Windows;
 using View = Autodesk.Revit.DB.View;
+using Autodesk.Windows;
 
 namespace R10PanelButtomTestNameSpace
 {
@@ -19,16 +20,56 @@ namespace R10PanelButtomTestNameSpace
     {
         public Autodesk.Revit.UI.Result OnStartup(UIControlledApplication application)
         {
+            //创建新的标签页
+            application.CreateRibbonTab("我的工具"); //
+
             //添加一个新的ribbon面板
-            RibbonPanel ribbonPanel = application.CreateRibbonPanel("NewRibbonPanel");
+            Autodesk.Revit.UI.RibbonPanel ribbonPanel = application.CreateRibbonPanel("我的工具", "New1");
 
             //在新的面板上添加一个按钮
             PushButton pushButton = ribbonPanel.AddItem(
-                    new PushButtonData("HelloRevit",
-                        "helloRevit",
+                    new PushButtonData("AddinManager",
+                        "Manual Mode",
                         @"D:\Revit 2019.1 SDK\Add-In Manager\AddInManager.dll",
                         "AddInManager.CAddInManager"))
                 as PushButton;
+            
+
+            //添加第二个个新的ribbon面板
+            Autodesk.Revit.UI.RibbonPanel ribbonPanel2 = application.CreateRibbonPanel("我的工具", "New2");
+
+            //在新的面板上添加一个按钮
+            PushButton pushButton2 = ribbonPanel2.AddItem(
+                    new PushButtonData("AddinManager",
+                        "Faceless",
+                        @"D:\Revit 2019.1 SDK\Add-In Manager\AddInManager.dll",
+                        "AddInManager.CAddInManagerFaceless"))
+                as PushButton;
+
+            #region 标签设置到最前端
+
+            Autodesk.Windows.RibbonControl ribbon = Autodesk.Windows.ComponentManager.Ribbon;
+
+            Autodesk.Windows.RibbonTab rt = null;
+
+            foreach (Autodesk.Windows.RibbonTab tab in ribbon.Tabs)
+
+            {
+                if (tab.Name == "我的工具")
+
+                {
+                    rt = tab;
+
+                    ribbon.Tabs.Remove(tab);
+
+                    break;
+                }
+            }
+
+            ribbon.Tabs.Insert(0, rt);
+
+            #endregion
+
             return Result.Succeeded;
         }
 
---
+++ DotNetRevit/TheCodeInBook/R0905WallCreat2.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Myclass;
+using TeacherTangClass;
+using View = Autodesk.Revit.DB.View;
+using MyClass;
+using Form = Autodesk.Revit.DB.Form;
+
+namespace ExerciseProject
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    [Journaling(JournalingMode.UsingCommandData)]
+    class R0905WallCreat2 : IExternalCommand
+    {
+        /// <summary>
+        /// 代码片段4-9
+        /// 创建墙(wall) 的方法3
+        /// Wall.Creat(Document doc, IList<Curve> profile,ElementId wallTypeId,ElementId levelId,boos structural,XYZ normal)
+        /// 最后一个参数normal指向墙的朝向. 如果墙的朝向不符合要求,可以使用 Wall.Flip()方法俩调整墙的朝向.
+        /// </summary>
+        /// <param name="commandData"></param>
+        /// <param name="message"></param>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Selection sel = uidoc.Selection;
+            View acView = uidoc.ActiveView;
+
+            Transaction ts = new Transaction(doc, "******");
+
+            try
+            {
+                ts.Start();
+
+                #region 代码4-9
+
+                ElementId levelId = new ElementId(694);
+                ElementId wallType = new ElementId(398);
+                IList<Curve> curves = new List<Curve>();
+                //创建第一面墙
+                XYZ[] vertexes = new XYZ[] {new XYZ(0, 0, 0), new XYZ(0, 200, 0), new XYZ(0, 0, 200) };
+
+                for (int i = 0; i < vertexes.Length; i++)
+                {
+                    if (i != vertexes.Length - 1)
+                    {
+                        curves.Add(Line.CreateBound(vertexes[i], vertexes[i + 1]));
+                    }
+                    else
+                    {
+                        curves.Add(Line.CreateBound(vertexes[i], vertexes[0]));
+                    }
+                }
+
+                Wall wall = null;
+                wall = Wall.Create(doc, curves, wallType, levelId, false, new XYZ(-1, 0, 0));
+
+                //创建第二面墙,朝向相反
+                curves.Clear();
+                vertexes = new XYZ[] {new XYZ(0, 0, 200), new XYZ(0, 200, 200), new XYZ(0, 200, 0)};
+                
+                for (int i = 0; i < vertexes.Length; i++)
+                {
+                    if (i != vertexes.Length-1)
+                    {
+                        curves.Add(Line.CreateBound(vertexes[i],vertexes[i+1]));
+                    }
+                    else
+                    {
+                        curves.Add(Line.CreateBound(vertexes[i],vertexes[0]));
+                    }
+                }
+                wall = Wall.Create(doc, curves, wallType, levelId, false, new XYZ(1, 0, 0));
+
+                TaskDialog.Show("tips", "成功");
+
+
+
+                #endregion
+
+                ts.Commit();
+            }
+            catch (Exception)
+            {
+                if (ts.GetStatus() == TransactionStatus.Started)
+                {
+                    ts.RollBack();
+                }
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
