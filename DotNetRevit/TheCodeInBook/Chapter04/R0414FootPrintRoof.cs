d90a035cc998708ab80e67268aff7870b473a13d
295665210 <295665210@qq.com>
2020-01-16 15:12:25 +0800

    2020年1月16日15:12:24

--- DotNetRevit/TheCodeInBook/Chapter04/R0414FootPrintRoof.cs
+++ DotNetRevit/TheCodeInBook/Chapter04/R0414FootPrintRoof.cs
@@ -25,7 +25,7 @@ namespace RevitDevelopmentFoundation.Chapter04
     class R0414FootPrintRoof : IExternalCommand
     {
         /// <summary>
-        /// 代码片段4-14 创建家具
+        /// 代码片段4-14 FootPrintRoof
         /// </summary>
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -41,31 +41,29 @@ namespace RevitDevelopmentFoundation.Chapter04
             {
                 ts.Start();
                 // 创建迹线屋顶前准备参数
-                Level level =doc.GetElement(new ElementId(341705)) as Level;
-                RoofType roofType =doc.GetElement(new ElementId(337)) as RoofType;
-                CurveArray curveArray =new CurveArray();
+                Level level = doc.GetElement(new ElementId(341705)) as Level;
+                RoofType roofType = doc.GetElement(new ElementId(337)) as RoofType;
+                CurveArray curveArray = new CurveArray();
                 //屋顶外边框
-                curveArray.Append(Line.CreateBound(new XYZ(0,0,0),new XYZ(30,0,0) ));
-                curveArray.Append(Line.CreateBound(new XYZ(30,0,0),new XYZ(30,30,0) ));
-                curveArray.Append(Line.CreateBound(new XYZ(30,30,0),new XYZ(0,30,0) ));
-                curveArray.Append(Line.CreateBound(new XYZ(0,30,0),new XYZ(0,0,0)));
+                curveArray.Append(Line.CreateBound(new XYZ(0, 0, 0), new XYZ(30, 0, 0)));
+                curveArray.Append(Line.CreateBound(new XYZ(30, 0, 0), new XYZ(30, 30, 0)));
+                curveArray.Append(Line.CreateBound(new XYZ(30, 30, 0), new XYZ(0, 30, 0)));
+                curveArray.Append(Line.CreateBound(new XYZ(0, 30, 0), new XYZ(0, 0, 0)));
                 //在中间添加洞口
-              curveArray.Append(Line.CreateBound(new XYZ(5,5,0),new XYZ(5,15,0) ));
-              curveArray.Append(Line.CreateBound(new XYZ(5,15,0),new XYZ(15,5,0)) );
-              curveArray.Append(Line.CreateBound(new XYZ(15,5,0),new XYZ(5,5,0) ));
+                curveArray.Append(Line.CreateBound(new XYZ(5, 5, 0), new XYZ(5, 15, 0)));
+                curveArray.Append(Line.CreateBound(new XYZ(5, 15, 0), new XYZ(15, 5, 0)));
+                curveArray.Append(Line.CreateBound(new XYZ(15, 5, 0), new XYZ(5, 5, 0)));
                 //创建屋顶
                 ModelCurveArray modelCurveArray = new ModelCurveArray();
                 FootPrintRoof roof = doc.Create.NewFootPrintRoof(curveArray, level, roofType, out modelCurveArray);
                 //设置屋顶坡度
                 ModelCurve curve1 = modelCurveArray.get_Item(0);
                 ModelCurve curve3 = modelCurveArray.get_Item(2);
-                roof.set_DefinesSlope(curve1,true);
-                roof.set_SlopeAngle(curve1,0.5);
+                roof.set_DefinesSlope(curve1, true);
+                roof.set_SlopeAngle(curve1, 0.5);
 
-                roof.set_DefinesSlope(curve3,true);
-                roof.set_SlopeAngle(curve3,1.6);
-
-
+                roof.set_DefinesSlope(curve3, true);
+                roof.set_SlopeAngle(curve3, 1.6);
 
                 ts.Commit();
             }
