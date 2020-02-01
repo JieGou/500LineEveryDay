e6891a6a0e19ef667320d37d9cf4907fb3e7a934
295665210 <295665210@qq.com>
2020-02-01 16:56:47 +0800

    mysql连接测试案例

--- DotNetRevit/TheCodeInJFeast/Code002.cs
+++ DotNetRevit/TheCodeInJFeast/Code002.cs
@@ -3,7 +3,6 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Mechanical;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
-
 using View = Autodesk.Revit.DB.View;
 
 
@@ -12,10 +11,8 @@ namespace TheCodeInJFeast
     [Transaction((TransactionMode.Manual))]
     [Regeneration(RegenerationOption.Manual)]
     [Journaling(JournalingMode.NoCommandData)]
-
     /* https://www.bilibili.com/video/av60526429?p=5              
-             */
-
+    */
     class Code002 : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
@@ -26,8 +23,7 @@ namespace TheCodeInJFeast
             Selection sel = uidoc.Selection;
             View acview = uidoc.ActiveView;
 
-
-            FilteredElementCollector collector =new FilteredElementCollector(doc);
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
 
             // collector.OfClass(typeof(Wall));
 
@@ -37,14 +33,10 @@ namespace TheCodeInJFeast
 
             collector.WherePasses(new ElementClassFilter(typeof(Duct)));
 
-
-
-
             Transaction ts = new Transaction(doc, "**");
 
             ts.Start();
 
-         
             ts.Commit();
             return Result.Succeeded;
         }
--- DotNetRevit/TheCodeInJFeast/RevitLinkToMysql.cs
+++ DotNetRevit/TheCodeInJFeast/RevitLinkToMysql.cs
@@ -9,13 +9,13 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using MySql.Data.MySqlClient;
 
-namespace Revit_Mysql
+namespace TheCodeInJFeast
 {
     /* ————————————————
     版权声明：本文为CSDN博主「黑夜の骑士」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
     原文链接：https://blog.csdn.net/birdfly2015/article/details/87923301
 
-        这是一个从mysql数据库读取数据的案例.
+        这是一个从mysql数据库读取数据的测试案例.
     */
 
 
@@ -27,23 +27,26 @@ namespace Revit_Mysql
             Document doc = commandData.Application.ActiveUIDocument.Document;
 
             //输入自己的Mysql账号（一般默认为root）、密码以及数据库名称        
-            String connetStr = "server=127.0.0.1;port=3306;user=root;password=123456;database=revit_wujinqian;";
-
-
+            String connetStr = "server=127.0.0.1;port=3306;user=root;password=123456;database=mystudytest;";
             MySqlConnection conn = new MySqlConnection(connetStr);
 
             conn.Open(); //打开通道，建立连接
-            string sql = "select * from 柱";
-            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            string sqlCmd = "select * from student";
+            MySqlCommand cmd = new MySqlCommand(sqlCmd, conn);
+
             MySqlDataReader reader = cmd.ExecuteReader(); //执行ExecuteReader()返回一个MySqlDataReader对象
+
+            string strShow = null;
 
             while (reader.Read()) //遍历每一行
             {
                 //输出数据库结果，列既可以通过列号，也可以通过列名来得到
                 //针对不通类型参数，采用相对应的reader.get方法
-                TaskDialog.Show("连接Mysql数据库", reader.GetString("ID"));
+                strShow += reader.GetString("student_id") + ":" + reader.GetInt32("age").ToString() + "\n";
             }
 
+            TaskDialog.Show("连接Mysql数据库", strShow);
             return Result.Succeeded;
         }
     }
