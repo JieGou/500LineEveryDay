86a2bcca46676d19b0230fdf2baa8c7c82d24cbe
295665210 <295665210@qq.com>
2020-02-03 21:06:38 +0800

    桁架钢筋2

---
+++ DotNetRevit/Other/Truss/SweepRefCode.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB.Structure;
+
+
+namespace Truss
+{
+    [Journaling(JournalingMode.NoCommandData)]
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class SweepRefCode : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document revitDoc = commandData.Application.ActiveUIDocument.Document;//获取文档
+            Application revitApp = commandData.Application.Application;//获取应用程序
+            CreatSweepSolid(revitDoc);
+            return Result.Succeeded;
+        }
+
+        public void CreatSweepSolid(Document revitDoc)
+        {
+            //获取模型线列表
+            FilteredElementCollector modelCurveCollector = new FilteredElementCollector(revitDoc);
+            List<ModelCurve> modelCurveList =
+                modelCurveCollector.OfCategory(BuiltInCategory.OST_Lines).OfClass(typeof(CurveElement)).ToList()
+                .ConvertAll(x => x as ModelCurve);//注意此处的typeof(CurveElement)，不能用ModelCurve，会报错
+            foreach (ModelCurve modelCurve in modelCurveList)
+            {
+                //第一步，创建一个新的族文件
+                Document familyDoc = revitDoc.Application.NewFamilyDocument
+                    (@"C:\ProgramData\Autodesk\RVT 2019\Family Templates\Chinese\公制常规模型.rft");
+                //现在就是族环境了，需要对族进行操作在此处进行
+                //获取pathList,得到扫略路径 
+                /*  对于项目环境中的模型线，不可以直接引用。需要根据已有模型线，在族环境中重新创建
+                ReferenceArray path = new ReferenceArray();
+                Reference reference = modelCurve.GeometryCurve.Reference;
+                path.Append(reference);
+                */
+                //定义扫略截面SweepProfile,此处无需生成模型线的！直接用Curve即可
+                Line line1 = Line.CreateBound(new XYZ(0, 0, 0), new XYZ(40 / 304.8, 0, 0));
+                Line line2 = Line.CreateBound(new XYZ(40 / 304.8, 0, 0), new XYZ(40 / 304.8, 6 / 304.8, 0));
+                Line line3 = Line.CreateBound(new XYZ(40 / 304.8, 6 / 304.8, 0), new XYZ(0, 6 / 304.8, 0));
+                Line line4 = Line.CreateBound(new XYZ(0, 6 / 304.8, 0), new XYZ(0, 0, 0));
+                CurveArray curveArray = new CurveArray();
+                curveArray.Append(line1);
+                curveArray.Append(line2);
+                curveArray.Append(line3);
+                curveArray.Append(line4);
+                CurveArrArray curveArrArray = new CurveArrArray();
+                curveArrArray.Append(curveArray);
+                SweepProfile sweepProfile = familyDoc.Application.Create.NewCurveLoopsProfile(curveArrArray);
+                Transaction transaction1 = new Transaction(familyDoc, "SweepProfile");//注意这里的Document用的是族doc,而不是revitDoc                
+                transaction1.Start();
+                //在族环境中，重新创建草图平面、模型线
+                Plane plane = modelCurve.SketchPlane.GetPlane();
+                SketchPlane sketchPlane = SketchPlane.Create(familyDoc, plane);
+                ModelCurve modelCurvePath = familyDoc.FamilyCreate.NewModelCurve(modelCurve.GeometryCurve, sketchPlane);
+                /*
+                SketchPlane sketchPlane = modelCurve.SketchPlane;
+                ModelCurve modelCurvePath = familyDoc.FamilyCreate.NewModelCurve(modelCurve.GeometryCurve, sketchPlane);             
+                */
+                //定义扫略路径path
+                ReferenceArray path = new ReferenceArray();
+                path.Append(modelCurvePath.GeometryCurve.Reference);
+                //创建扫略放样
+                familyDoc.FamilyCreate.NewSweep(true, path, sweepProfile, 0, ProfilePlaneLocation.Start);
+                transaction1.Commit();
+                //对族的编辑完成后，加载族文件到项目文件
+                Family loadingFamily = familyDoc.LoadFamily(revitDoc);
+                //在项目中实例化该族
+                FamilySymbol familySymbol = revitDoc.GetElement(loadingFamily.GetFamilySymbolIds().First()) as FamilySymbol;
+                Transaction transaction2 = new Transaction(revitDoc, "CreatFamilyInstance");
+                transaction2.Start();
+                familySymbol.Activate();//必须要激活
+                revitDoc.Create.NewFamilyInstance(XYZ.Zero, familySymbol, StructuralType.NonStructural);
+                transaction2.Commit();
+            }
+        }
+        public class ProjectFamLoadOption : IFamilyLoadOptions//项目中创建族，需要用到此接口
+        {
+            public bool OnFamilyFound(bool familyInUse, out bool overwriteParameterValues)
+            {
+                overwriteParameterValues = true;
+                return true;
+            }
+
+            public bool OnSharedFamilyFound(Family sharedFamily, bool familyInUse, out FamilySource source, out bool overwriteParameterValues)
+            {
+                source = FamilySource.Project;
+                overwriteParameterValues = true;
+                return true;
+            }
+        }
+    }
+}
---
+++ DotNetRevit/Other/Truss/SweepRefCode3.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Linq;
+using Autodesk.Revit.Creation;
+using Application = Autodesk.Revit.ApplicationServices.Application;
+using Document = Autodesk.Revit.DB.Document;
+
+namespace Truss
+{
+    /// <summary>
+    /// code in autodesk develop foundation  code 7-17
+    /// </summary>
+    [TransactionAttribute(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    public class SweepRefCode3 : IExternalCommand
+    {
+        // private Document doc;
+        private double length;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+            // UIApplication uiApp = commandData.Application;
+            // Application app = uiApp.Application;
+
+            Transaction ts = new Transaction(doc, "******");
+            ts.Start();
+
+            //定义桁架钢筋长度
+            length = 2000 / 304.8;
+            //创建桁架上弦钢筋
+            CreateUpSteel(doc, length);
+            CreateDownSteel(doc, length);
+
+            ts.Commit();
+
+            return Result.Succeeded;
+        }
+
+        private void CreateUpSteel(Autodesk.Revit.DB.Document doc, double length)
+        {
+            CurveArrArray arrarr = new CurveArrArray();
+            CurveArray arr = new CurveArray();
+
+            //创建路径平面: CreateSketchPlane  参见Extrusion的例子
+            Autodesk.Revit.DB.XYZ normal = XYZ.BasisZ;
+            SketchPlane sketchPlane = SketchPlane.Create(doc, Plane.CreateByNormalAndOrigin(normal, XYZ.Zero));
+
+            //创建用于放样的轮廓,这里创建轮廓线在生成轮廓的方式
+            XYZ pnt1 = new XYZ(0, 0, 0);
+
+            double UpSteelDiameter = 8 / 304.8;
+            arr.Append(Arc.Create(pnt1, UpSteelDiameter / 2, 0.0d, 360.0d, XYZ.BasisX, XYZ.BasisY));
+            arrarr.Append(arr);
+            SweepProfile profile = doc.Application.Create.NewCurveLoopsProfile(arrarr);
+
+            //创建用于放样的路径,该路径包括1条线段.
+            length = this.length;
+            XYZ pnt2 = new XYZ(pnt1.Y + length, 0, 0);
+
+            Curve curve1 = Line.CreateBound(pnt1, pnt2);
+            CurveArray curvesPath = new CurveArray();
+            curvesPath.Append(curve1);
+
+            //利用轮廓和路径创建放样,轮廓线位于路径的第二条线段的中心点
+            Sweep sweep1 = doc.FamilyCreate.NewSweep(true, curvesPath, sketchPlane, profile, 0,
+                                                     ProfilePlaneLocation.Start);
+
+            XYZ transAxis = new XYZ(0, 0, 76 / 304.8);
+            ElementTransformUtils.MoveElement(doc, sweep1.Id, transAxis);
+        }
+
+        private void CreateDownSteel(Autodesk.Revit.DB.Document doc, double length)
+        {
+            CurveArrArray arrarr = new CurveArrArray();
+            CurveArray arr = new CurveArray();
+
+            //创建路径工作平面: CreateSketchPlane  参见Extrusion的例子
+            Autodesk.Revit.DB.XYZ normal = XYZ.BasisZ;
+            SketchPlane sketchPlane = SketchPlane.Create(doc, Plane.CreateByNormalAndOrigin(normal, XYZ.Zero));
+
+            //创建用于放样的轮廓,这里创建轮廓线在生成轮廓的方式
+            XYZ pnt1 = new XYZ(0, 0, 0);
+
+            double DownSteelDiameter = 6 / 304.8;
+
+            arr.Append(Arc.Create(pnt1, DownSteelDiameter / 2, 0.0d, 360.0d, XYZ.BasisX, XYZ.BasisY));
+            arrarr.Append(arr);
+            SweepProfile profile = doc.Application.Create.NewCurveLoopsProfile(arrarr);
+
+            //创建用于放样的路径,该路径包括1条线段.
+            length = this.length;
+            XYZ pnt2 = new XYZ(pnt1.Y + length, 0, 0);
+
+            Curve curve1 = Line.CreateBound(pnt1, pnt2);
+            CurveArray curvesPath = new CurveArray();
+            curvesPath.Append(curve1);
+
+            //利用轮廓和路径创建放样,轮廓线位于路径的第二条线段的中心点
+            Sweep sweep1 = doc.FamilyCreate.NewSweep(true, curvesPath, sketchPlane, profile, 0,
+                                                     ProfilePlaneLocation.Start);
+            //定义桁架宽度
+            double TrusWidth = 80 / 304.8;
+            XYZ transAxis1 = new XYZ(0, TrusWidth / 2 - DownSteelDiameter/2, DownSteelDiameter/2);
+            var sweep2 = ElementTransformUtils.CopyElement(doc, sweep1.Id, transAxis1);
+
+            XYZ transAxis2 = new XYZ(0, -transAxis1.Y, transAxis1.Z);
+            var sweep3 = ElementTransformUtils.CopyElement(doc, sweep1.Id, transAxis2);
+
+            doc.Delete(sweep1.Id);
+        }
+    }
+
+    // private void CreateSweep(Autodesk.Revit.DB.Document doc)
+    // {
+    // CurveArrArray arrarr = new CurveArrArray();
+    // CurveArray arr = new CurveArray();
+    //
+    // //创建路径平面: CreateSketchPlane  参见Extrusion的例子
+    // Autodesk.Revit.DB.XYZ normal = XYZ.BasisZ;
+    // SketchPlane sketchPlane = SketchPlane.Create(doc, Plane.CreateByNormalAndOrigin(normal, XYZ.Zero));
+    //
+    // //创建用于放样的轮廓,这里创建轮廓线在生成轮廓的方式
+    // XYZ pnt1 = new XYZ(0, 0, 0);
+    // XYZ pnt2 = new XYZ(2, 0, 0);
+    // XYZ pnt3 = new XYZ(1, 1, 0);
+    // arr.Append(Arc.Create(pnt2, 1.0d, 0.0d, 180.0d, XYZ.BasisX, XYZ.BasisY));
+    // arr.Append(Arc.Create(pnt1, pnt3, pnt2));
+    // arrarr.Append(arr);
+    // SweepProfile profile = doc.Application.Create.NewCurveLoopsProfile(arrarr);
+    //
+    // //创建用于放样的路径,该路劲包括两条线段.
+    // XYZ pnt4 = new XYZ(0, 0, 0);
+    // XYZ pnt5 = new XYZ(10, 0, 0);
+    // XYZ pnt6 = new XYZ(10, 10, 0);
+    // Curve curve1 = Line.CreateBound(pnt4, pnt5);
+    // Curve curve2 = Line.CreateBound(pnt5, pnt6);
+    // CurveArray curvesPath = new CurveArray();
+    // curvesPath.Append(curve1);
+    // curvesPath.Append(curve2);
+    //
+    // //利用轮廓和路径创建放样,轮廓线位于路径的第二条线段的中心点
+    // Sweep sweep1 = doc.FamilyCreate.NewSweep(true, curvesPath, sketchPlane, profile, 1,
+    //                                          ProfilePlaneLocation.MidPoint);
+    //
+    // Line axis = Line.CreateBound(pnt4, pnt5);
+    // ElementTransformUtils.RotateElement(doc, sweep1.Id, axis, Math.PI / 4);
+    // }
+}
