d489419760bf157bf2fa351fecd789612679e873
295665210 <295665210@qq.com>
2020-02-22 19:00:06 +0800

    2

---
+++ DotNetFoundation/CodeFrame/LearningHardCSharpNote/Chaper8Delegate/Code0801.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFrame.LearningHardCSharpNote.Chaper8Delegate
+{
+    class Code0801
+    {
+        //1 使用delegate关键字来定义一个委托类型
+        delegate void MyDelegate(int para1, int para2);
+
+        static void Main(string[] args)
+        {
+            //2 声明委托变量d
+            MyDelegate d;
+            //3 实例化委托类型,传递的方法也可以是静态方法,这里传递的是实例方法.
+            d = new MyDelegate(new Code0801().Add);
+
+            //4 委托类型作为参数传递给另一个方法
+            MyMethod(d);
+            Console.Read();
+
+        }
+
+        void Add(int para1, int para2)
+        {
+            int sum = para1 + para2;
+            Console.WriteLine("两个数的和为:" + sum);
+        }
+
+        private static void MyMethod(MyDelegate myDelegate)
+        {
+            //5 在方法中调用委托
+            myDelegate(1, 2);
+
+        }
+
+        
+    }
+}
---
+++ DotNetFoundation/CodeFrame/LearningHardCSharpNote/Chaper8Delegate/CodeP92.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFrame.LearningHardCSharpNote.Chaper8Delegate
+{
+    class CodeP92
+    {
+        public delegate void GreetingDelegate(string name);
+
+        static void Main(string[] args)
+        {
+            //引用委托之后
+            CodeP92 p =new CodeP92();
+            p.Greeting("张三",p.EnglishGreeting); 
+            //方法变成了一个参数传给了greeting方法,是通过将防greeting方法定义是,将参数设置为委托实现的.
+            p.Greeting("李四",p.ChineseGreeting);
+            Console.Read();
+        }
+
+        public void Greeting(string name, GreetingDelegate callback)
+        {
+            //调用委托
+            callback(name);
+        }
+
+        //英国人打招呼方法
+        public void EnglishGreeting(string name)
+        {
+            Console.WriteLine("Hello" + name);
+        }
+
+        //中国人打招呼方法
+        public void ChineseGreeting(string name)
+        {
+            Console.WriteLine("您好!" + name);
+        }
+    }
+}
