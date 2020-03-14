d63e2bc849ea520d1a4f64bc59931d02a0ad39c9
陈杰 <jiegou04@gmail.com>
2020-03-14 19:42:48 +0800

    通过设置Wpf为主窗口解决使用ALT+Tab出现插件对话框的问题

--- WPF/LearnWpfMVVM/CurvedBeam/Command/ExternalCommands.cs
+++ WPF/LearnWpfMVVM/CurvedBeam/Command/ExternalCommands.cs
@@ -1,19 +1,19 @@
-﻿using System;
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using LearnWpfMVVM.CurvedBeam.View;
+using LearnWpfMVVM.ExportFamilys;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
-using Autodesk.Revit.Attributes;
-using LearnWpfMVVM.CurvedBeam.View;
-using LearnWpfMVVM.ExportFamilys;
-
+using System.Windows.Interop;
 
 namespace LearnWpfMVVM.CurvedBeam.Command
 {
     [Transaction(TransactionMode.Manual)]
-    class ExternalCommands : IExternalCommand
+    internal class ExternalCommands : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -21,7 +21,14 @@ namespace LearnWpfMVVM.CurvedBeam.Command
             Document doc = uidoc.Document;
 
             CurvedBeamMainWindow mainWindow = new CurvedBeamMainWindow(commandData);
-          
+
+            //主父窗口为Revit
+            IWin32Window revitWindow = new JtWindowHandle(Autodesk.Windows.ComponentManager.ApplicationWindow);
+            WindowInteropHelper helper = new WindowInteropHelper(mainWindow)
+            {
+                Owner = revitWindow.Handle
+            };
+
             mainWindow.ShowDialog();
             return Result.Succeeded;
         }
---
+++ WPF/LearnWpfMVVM/JtWindowHandle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+namespace LearnWpfMVVM
+{
+    public class JtWindowHandle : IWin32Window
+    {
+        private IntPtr _hwnd;
+
+        public JtWindowHandle(IntPtr h)
+        {
+            //Debug.Assert(IntPtr.Zero != h, "expected non-null window handle");
+
+            _hwnd = h;
+        }
+
+        public IntPtr Handle
+        {
+            get
+            {
+                return _hwnd;
+            }
+        }
+    }
+}
